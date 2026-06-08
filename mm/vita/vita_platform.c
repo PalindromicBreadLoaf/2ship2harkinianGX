a46a721d9f27cdac6b36c8403ff54dfd9f125068
PalindromicBreadLoaf <palindromicbreadloaf@tuta.com>
2026-06-08 17:08:46 -0400

    Stub a bunch of functions temporarily

--- mm/vita/vita_platform.c
+++ mm/vita/vita_platform.c
@@ -1,3 +1,37 @@
 // PS Vita platform glue.
+#include <vitaGL.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <errno.h>
 
+// This looks like a good number. I don't really know what I'm doing.
 unsigned int _newlib_heap_size_user = 192 * 1024 * 1024;
+
+// TODO: Actually do this? Should work fine without.
+void glDetachShader(GLuint program, GLuint shader) {
+    (void)program;
+    (void)shader;
+}
+
+pid_t waitpid(pid_t pid, int* status, int options) {
+    (void)pid;
+    (void)status;
+    (void)options;
+    errno = ENOSYS;
+    return -1;
+}
+
+int execvp(const char* file, char* const argv[]) {
+    (void)file;
+    (void)argv;
+    errno = ENOSYS;
+    return -1;
+}
+
+int execl(const char* path, const char* arg, ...) {
+    (void)path;
+    (void)arg;
+    errno = ENOSYS;
+    return -1;
+}
